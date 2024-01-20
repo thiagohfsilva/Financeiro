28765b312c2bf07cab6f13a495a60290ff481f78
kaleno <7h3cl0wn@gmail.com>
2024-01-20 03:30:52 -0300

    [Inclusao de funcoes de salvar e carregar dados]
       - Inclusao de funcoes de carregar e salvar dados de FinancialYear.
       - Inclusao de ID em cada pagamento.
       - Movido data.json para a raiz do projeto.

--- libs/transaction.py
+++ libs/transaction.py
@@ -2,12 +2,13 @@ import json
 from tabulate import tabulate
 
 class Transaction:
-    def __init__(self, p_value : int, p_periodic: bool,
+    def __init__(self, p_id: int, p_value : int, p_periodic: bool,
                  p_note : str, p_done : bool, p_expiration_day : int, p_resp : str,
                  p_type : str) -> None :
         '''
         Classe destinada a descrever uma transação:
 
+        @param p_id: Identificador único da transação.
         @param p_value: Valor da transação.
         @param p_periodic: True caso se repita todos os meses.
         @param p_note: Campo de cometario para outras explicações.
@@ -16,6 +17,7 @@ class Transaction:
         @param p_expiration_day: Data de vencimento da transação.
         @param p_resp: Responsável por realizar a transação.
         '''
+        self.id = p_id
         self.value = p_value
         self.periodic = p_periodic
         self.note = p_note
@@ -65,11 +67,12 @@ class Month:
         '''
         Exibe as transações do mês em forma de tabela.
         '''
-        headers = ["Valor", "Periódica", "Nota", "Tipo", "Concluída", "Dia de Vencimento", "Responsável"]
+        headers = ["ID", "Valor", "Periódica", "Nota", "Tipo", "Concluída", "Dia de Vencimento", "Responsável"]
         table_data = []
 
         for transaction in self.transaction_list:
             table_data.append([
+                transaction.id,
                 transaction.value,
                 "Sim" if transaction.periodic else "Não",
                 transaction.note,
@@ -89,6 +92,7 @@ class FinancialYear:
 
         @param json_file_path: Caminho para o arquivo JSON que contém os dados.
         '''
+        self.json_file_path = json_file_path
         self.month_list = self.load_data_from_json(json_file_path)
 
     def load_data_from_json(self, json_file_path):
@@ -98,27 +102,75 @@ class FinancialYear:
         @param json_file_path: Caminho para o arquivo JSON que contém os dados.
         @return: Lista de objetos Month.
         '''
-        #try:
-        with open(json_file_path, 'r') as file:
-            data = json.load(file)
-            # Verifica se a chave 'months' está presente no JSON
-            if 'months' in data:
-                month_list = []
-                for month_data in data['months']:
-                    # Cria objetos Month a partir dos dados do JSON
-                    month = Month(p_mes=month_data['mes'],
-                                    p_ano=month_data['ano'],
-                                    p_transaction_list=month_data.get('transaction_list', []))
-                    month_list.append(month)
-                return month_list
-            else:
-                raise ValueError("Chave 'months' não encontrada no JSON.")
-        #except FileNotFoundError:
-        #    raise FileNotFoundError(f"O arquivo JSON em {json_file_path} não foi encontrado.")
-        #except json.JSONDecodeError:
-        #    raise ValueError(f"Erro ao decodificar o JSON no arquivo {json_file_path}.")
+        try:
+            with open(json_file_path, 'r') as file:
+                data = json.load(file)
+                # Verifica se a chave 'months' está presente no JSON
+                if 'months' in data:
+                    month_list = []
+                    for month_data in data['months']:
+                        # Cria objetos Month a partir dos dados do JSON
+                        transaction_list = []
+                        for transaction_data in month_data.get('transaction_list', []):
+                            transaction_list +=[Transaction(transaction_data["id"],
+                                                            transaction_data["value"],
+                                                            transaction_data["periodic"],
+                                                            transaction_data["note"],
+                                                            transaction_data["done"],
+                                                            transaction_data["expiration_day"],
+                                                            transaction_data["resp"],
+                                                            transaction_data["type"])]
+                        month = Month(p_mes=month_data['mes'],
+                                      p_ano=month_data['ano'],
+                                      p_transaction_list=transaction_list)
+                        month_list.append(month)
+                    return month_list
+                else:
+                    raise ValueError("Chave 'months' não encontrada no JSON.")
+        except FileNotFoundError:
+            raise FileNotFoundError(f"O arquivo JSON em {json_file_path} não foi encontrado.")
+        except json.JSONDecodeError:
+            raise ValueError(f"Erro ao decodificar o JSON no arquivo {json_file_path}.")
+    
+    def save_data_to_json(self):
+        '''
+        Salva os dados da lista de meses em um arquivo JSON.
+        '''
+        data = {"months": []}
+
+        for month in self.month_list:
+            month_data = {
+                "mes": month.mes,
+                "ano": month.ano,
+                "transaction_list": []
+            }
+
+            for transaction in month.transaction_list:
+                transaction_data = {
+                    "id": transaction.id,
+                    "value": transaction.value,
+                    "periodic": transaction.periodic,
+                    "note": transaction.note,
+                    "type": transaction.type,
+                    "done": transaction.done,
+                    "expiration_day": transaction.expiration_day,
+                    "resp": transaction.resp
+                }
+                month_data["transaction_list"].append(transaction_data)
+
+            data["months"].append(month_data)
+
+        try:
+            with open(self.json_file_path, 'w') as file:
+                json.dump(data, file, indent=2)
+                print(f"Dados salvos com sucesso no arquivo JSON: {self.json_file_path}")
+        except IOError:
+            print(f"Erro ao salvar os dados no arquivo JSON: {self.json_file_path}")
 
 # Exemplo de uso da classe FinancialYear
 financial_year_instance = FinancialYear('data.json')
+for i in range(len(financial_year_instance.month_list)):
+    financial_year_instance.month_list[i].display_transactions_table()
+financial_year_instance.save_data_to_json()
 
 
